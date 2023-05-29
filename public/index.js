589c1fe1e92b2a69519613d29ec9e0ca09c39693
txxxc <admin@tochka.lv>
2023-05-30 00:14:56 +0200

    start

---
+++ public/index.js
@@ -0,0 +1,75 @@
+// Initialize and add the map
+let map;
+let marker;
+async function initMap() {
+  const position = {
+    lat: 38.6434757,
+    lng: 0.0082797
+  };
+  const {
+    Map
+  } = await google.maps.importLibrary("maps");
+  const {
+    AdvancedMarkerElement
+  } = await google.maps.importLibrary("marker");
+  const beachFlagImg = document.createElement("img");
+  beachFlagImg.src =
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+
+  const image =
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+  map = new Map(document.getElementById("map"), {
+    zoom: 14,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+  marker = new google.maps.Marker({
+    map: map,
+    content: beachFlagImg,
+    position: position,
+    title: "Jorik",
+  });
+}
+initMap();
+
+fetchTJ();
+
+function placeLocationOnMap() {
+  //const location = checkCurrentLocation();
+}
+async function fetchTJ() {
+  fetch(`/tj`, {
+      method: 'GET',
+    })
+    .then(res => res.json())
+    .catch(function (err) {
+      return;
+    })
+    .then(async function (res) {
+      await sleep(3000);
+      if (res) {
+        if (Object.keys(res).length > 0) {
+          const pos = res.current.geometry.coordinates;
+
+          console.log(pos);
+          var latlng = new google.maps.LatLng(pos[1], pos[0]);
+          marker.setPosition(latlng);
+          map.setCenter(latlng);
+        }
+      }
+      console.log(`...`)
+      fetchTJ();
+    });
+}
+
+function sleep(ms) {
+  return new Promise(resolveFunc => setTimeout(resolveFunc, ms));
+}
+async function checkCurrentLocation() {
+  return new Promise(resolve => {
+    const sir = setInterval(() => {
+      console.log(`test`);
+      resolve(`test`);
+    }, 500);
+  })
+}
---
+++ tj.js
@@ -0,0 +1,43 @@
+var express = require('express');
+const fs = require('fs');
+var cors = require('cors');
+var app = express();
+const path = require('path');
+const bodyParser = require("body-parser")
+var PORT = 8081;
+app.use(cors());
+app.use(express.static(path.join(__dirname, '/public')))
+app.use(bodyParser.json())
+function readJSON(callback) {
+    fs.readFile('data.json', "utf8", function(err, result) {
+        if (err) callback(err);
+        callback(null, JSON.parse(result));
+    });
+}
+app.locals.pretty = true;
+
+let currentLocation = {};
+
+app.post("/hook", (req, res) => {
+  console.log(req.body) // Call your action on the request here
+  currentLocation = req.body;
+  res.status(200).end() // Responding is important
+})
+app.get('/tj', (request, response) => {
+    currentLocation
+    response.send(currentLocation);
+    // readJSON((err, nameContent) => {
+    //     if(err) {
+    //         response.status(500).send(err);
+    //         return;
+    //     }
+    //     response.send(nameContent);
+    // })
+
+});
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname, '/index.html'));
+});
+app.listen(PORT,function(){
+    console.log('Server started at http://localhost:' + PORT);
+})
